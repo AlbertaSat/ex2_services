5c3b50265a22d83a9a20cb59a9492a32318fe913
Andrew Rooney <arrooney@ualberta.ca>
2020-06-07 17:25:51 -0600

    add header info to main

--- main.c
+++ main.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015  University of Alberta
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+/**
+ * @file main.c
+ * @author Andrew Rooney
+ * @date 2020-06-06
+ */
+
 #include <FreeRTOS.h>
 #include <csp/csp.h>
 #include <csp/interfaces/csp_if_zmqhub.h>
